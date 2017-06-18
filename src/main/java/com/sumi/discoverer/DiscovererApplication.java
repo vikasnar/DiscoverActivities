e361a66507a421898f8ac100e9d5878d131d448a
VikasN <vikas.n649@gmail.com>
2017-06-17 19:13:51 -0700

    Setting up project

---
+++ src/main/java/com/sumi/discoverer/DiscovererApplication.java
@@ -0,0 +1,12 @@
+package com.sumi.discoverer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscovererApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscovererApplication.class, args);
+	}
+}
